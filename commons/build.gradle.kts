96fc48bc721d191b863a5f0764168a5d5e4616ce
mmochiiiiiii <unardngarm_t@su.ac.th>
2026-04-02 15:50:44 +0700

    Add dependencies for configurate-yaml and geantyref

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
+    implementation("org.spongepowered:configurate-yaml:4.1.2")
+    implementation("io.leangen.geantyref:geantyref:1.3.15")
     implementation("com.google.code.gson:gson:2.10.1")
 }
