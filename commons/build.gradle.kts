766b303ddd06aeb76345de8eb81f3c6809e0aa24
mmochiiiiiii <unardngarm_t@su.ac.th>
2026-04-04 20:04:44 +0700

    Add JUnit 5 dependencies and configure test logging in build.gradle.kts

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -2,4 +2,15 @@ dependencies {
     implementation("org.spongepowered:configurate-yaml:4.1.2")
     implementation("io.leangen.geantyref:geantyref:1.3.15")
     implementation("com.google.code.gson:gson:2.10.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
