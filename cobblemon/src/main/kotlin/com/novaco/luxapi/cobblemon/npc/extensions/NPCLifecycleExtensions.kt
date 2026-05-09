5de142332887f44fa9aff81dfee79aaec138cb57
mmochiiiiiii <unardngarm_t@su.ac.th>
2026-05-09 18:58:57 +0700

    Add NPCLifecycleExtensions for graceful NPC management in LuxAPI

---
+++ cobblemon/src/main/kotlin/com/novaco/luxapi/cobblemon/npc/extensions/NPCLifecycleExtensions.kt
@@ -0,0 +1,28 @@
+package com.novaco.luxapi.cobblemon.npc.extensions
+
+import com.novaco.luxapi.cobblemon.npc.manager.LuxNPCManager
+import net.minecraft.server.MinecraftServer
+
+/**
+ * Extension utility functions for managing LuxAPI NPCs gracefully.
+ */
+
+/**
+ * Despawns an NPC created by LuxAPI using its developer-assigned ID.
+ * * Example:
+ * ```
+ * server.despawnNPC("tutorial_guide_01")
+ * ```
+ * * @param customId The ID provided during the LuxNPCBuilder configuration.
+ * @return True if the NPC was removed, false if it could not be found.
+ */
+fun MinecraftServer.despawnNPC(customId: String): Boolean {
+    return LuxNPCManager.removeNPC(customId, this)
+}
+
+/**
+ * Checks if a specific LuxAPI NPC is currently registered and loaded.
+ */
+fun MinecraftServer.isNPCLoaded(customId: String): Boolean {
+    return LuxNPCManager.getNPC(customId, this) != null
+}
